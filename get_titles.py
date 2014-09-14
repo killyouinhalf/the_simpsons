930a6a034dfd2fe56fff10d9fd2c68fdfe5b5802
killyouinhalf <klabeau@gmail.com>
2014-09-14 00:48:51 -0400

    Getting libraries imported. I'm too damned tired...

--- get_titles.py
+++ get_titles.py
@@ -2,3 +2,9 @@ __author__ = 'klabeau'
 
 import requests
 from bs4 import BeautifulSoup
+
+r = requests.get("http://thetvdb.com/?tab=season&seriesid=71663&seasonid=2739&lid=7")
+text = r.text
+soup = BeautifulSoup(text)
+
+print (soup.odd)
